0d9e5b37a058c27fed0928f41e706a115205c14e
PREETHI-T0511 <preethitatikonda@gmail.com>
2026-07-23 21:50:44 +0530

    feat(database): initialize Prisma and create User model

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+
 const express = require("express"); //import express, bring express into this file
 //Go into the node_modules folder, grab all the pre-built code tools from the Express package, 
 // and store them inside a variable called express so I can use them.
@@ -7,13 +7,17 @@ const app = express(); //created an instance
 //It fires up the Express tool and creates your actual 
 // web application object, naming it app. You will use this app 
 // variable to build your whole website or API.
+app.use((req,res,next)=>{
+    console.log(`[LOG] ${req.method} ${req.url}`);
+    next();
+});
 
-const PORT = process.env.PORT || 3000;
-//It sets a variable for the port number.
+//no need of port number here, cause server.js sets up the configuration
 
 app.get("/", (req, res) => {
     res.send("Hello from CodeForge Backend!"); //it sends a response back to the client
 });
+
 //It tells the server, Listen for anyone visiting the main home address (the / slash means the root homepage) 
 // using a standard web browser request (GET).The req (request) and res (response) are the inputs for
 // the action that follows.
@@ -25,6 +29,23 @@ app.get("/api/health", (req, res) => { //another route
         version: "1.0.0",
     });
 });
+
+
+app.get("/api/crash", (req, res) => {
+    throw new Error("Something exploded!");
+});
+
+app.use((err, req, res, next) => {
+    console.log("Error Middleware Reached!");
+
+    res.status(500).json({
+        success: false,
+        message: err.message
+    });
+});
+
+
+
 
 module.exports=app;
 //why?
